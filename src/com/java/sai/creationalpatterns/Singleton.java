81b32cb214d7592b15c0fc0b14f40b7b747fd4f8
Sai Krishna Palle <pallesp@amazon.com>
2018-12-14 22:21:26 -0800

    Singleton with LazyLoading

--- src/com/java/sai/creationalpatterns/Singleton.java
+++ src/com/java/sai/creationalpatterns/Singleton.java
@@ -1,11 +1,14 @@
 package com.java.sai.creationalpatterns;
 
 public class Singleton {
-	private static Singleton singleTon = new Singleton();
+	private static Singleton singleTon = null;
 	
 	private Singleton() {}
 	
 	public static Singleton getSingleton() {
+		if(null == singleTon) {
+		 singleTon	= new Singleton();
+		}
 		return singleTon;
 	}
 }
